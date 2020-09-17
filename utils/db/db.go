d61a1c1fdeaa5cc4ae75e977b7ac35fce4fd1eb2
Tarek Ali <tarekali15@outlook.com>
2020-09-17 12:58:55 -0500

    removed ping

--- utils/db/db.go
+++ utils/db/db.go
@@ -6,7 +6,6 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var Client *mongo.Client
@@ -15,10 +14,6 @@ func Open() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	Client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		panic(err)
-	}
-	err = Client.Ping(ctx, readpref.Primary())
 	if err != nil {
 		panic(err)
 	}
