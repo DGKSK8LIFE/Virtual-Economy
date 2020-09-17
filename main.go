9ae1fff05a0f9fdffdec0d368681bb25140b4d2a
Tarek Ali <tarekali15@outlook.com>
2020-09-17 09:16:44 -0500

    invocation

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "mutate"
+
 func main() {
-	
+	mutate.Setup()
 }
--- utils/mutate/mutate.go
+++ utils/mutate/mutate.go
@@ -1,7 +1,20 @@
 package mutate
 
-var ExchangeRate int64
+import "time"
 
+// Pool is the amount of money that is available globally
+var Pool int64
+
+// Setup starts the economy
 func Setup() {
-	ExchangeRate = 0 
+	Pool = 0
+	mutate()
+}
+
+func mutate() {
+	for {
+		// Artificial inflation; will probably make it temporary
+		Pool++
+		time.Sleep(1000)
+	}
 }
