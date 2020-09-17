2ec73b4a0b93b5f80811c698b30d5fc4856fd96f
Tarek Ali <tarekali15@outlook.com>
2020-09-17 12:45:21 -0500

    some db code; will expand later

--- utils/db/db.go
+++ utils/db/db.go
@@ -1,1 +1,17 @@
 package db
+
+import (
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Client *mongo.Client
+
+func Open() {
+	Client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatalf("can't connect to db: %s\n", err)
+	}
+}
