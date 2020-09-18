3b631aefca6caf889b61b54bf24dc604d4ae06ff
Tarek Ali <tarekali15@outlook.com>
2020-09-18 09:54:28 -0500

    working on Alltokens

--- utils/db/db.go
+++ utils/db/db.go
@@ -13,6 +13,9 @@ import (
 // Client of the db
 var Client *mongo.Client
 
+// collection
+var collection *mongo.Collection
+
 // Open opens mongodb/establishes connection
 func Open() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -21,6 +24,7 @@ func Open() {
 	if err != nil {
 		panic(err)
 	}
+	collection = Client.Database("economy").Collection("tokens")
 	defer func() {
 		if err = Client.Disconnect(ctx); err != nil {
 			panic(err)
@@ -31,7 +35,6 @@ func Open() {
 // CreateToken creates an individual token (currency of the virtual-economy)
 func CreateToken() error {
 	identifier := uuid.New()
-	collection := Client.Database("economy").Collection("tokens")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	_, err := collection.InsertOne(ctx, bson.M{"tokenid": identifier})
@@ -39,4 +42,24 @@ func CreateToken() error {
 		return err
 	}
 	return nil
+}
+
+func AllTokens() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	cur, err := collection.Find(ctx, bson.D{})
+	if err != nil {
+		return err
+	}
+	defer cur.Close(ctx)
+	for cur.Next(ctx) {
+		var result bson.M
+		err := cur.Decode(&result)
+		if err != nil {
+			return err
+		}
+	}
+	if err := cur.Err(); err != nil {
+		return err
+	}
 }
